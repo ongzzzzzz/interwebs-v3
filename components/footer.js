31359d6f3a50c0cd2febfce14e7bafd3e382c8bb
Ong Zhi Zheng <Fogeinator@users.noreply.github.com>
2021-07-08 08:12:18 +0800

    fix footer

--- components/footer.js
+++ components/footer.js
@@ -8,7 +8,7 @@ export default function Footer({}) {
 
     return (
         <div style={{ height: "50px", backgroundColor: "#272727" }}
-            className="w-full flex flex-row justify-center items-center text-base md:text-2xl text-white absolute bottom-0">
+            className="w-full flex flex-row justify-center items-center text-base md:text-2xl text-white bottom-0">
             <h1 className="text-xs md:text-lg text-right m-2">© 2021 Ong Zhi Zheng</h1>
             <div className="text-2xl flex flex-row justify-evenly items-center">
                 <Link href="https://github.com/Fogeinator/" target="_blank"><a>
