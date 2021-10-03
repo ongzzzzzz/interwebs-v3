e32628b15f705a850a7cb53dec17166cd84f432f
Ong Zhi Zheng <Fogeinator@users.noreply.github.com>
2021-10-03 19:46:46 +0800

    add firebase

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 
+import firebase from '../lib/firebase'
+
 // The following import prevents a Font Awesome icon server-side rendering bug,
 // where the icons flash from a very large icon down to a properly sized one:
 import '@fortawesome/fontawesome-svg-core/styles.css';
